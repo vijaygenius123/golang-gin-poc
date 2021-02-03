607c7beb86166936598ff15c7944da1f1d1bd9b7
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-02-03 20:53:25 +0530

    Added A Simple API

--- server.go
+++ server.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
-func main(){
-	fmt.Println("Hello World!")
+func main() {
+	fmt.Println("Starting")
+	server := gin.Default()
+
+	server.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "Hello",
+		})
+	})
+
+	server.Run(":8080")
 }
